557cc43e2e8db5fac4d04eef619f85df03e8c94c
ali.yucel <aliyucel.buyuk@gmail.com>
2025-04-11 07:52:23 +0300

    text updates

--- app/page.tsx
+++ app/page.tsx
@@ -159,8 +159,8 @@ export default function Home() {
         <section className="py-16 bg-gray-50">
           <div className="container mx-auto px-4">
             <div className="text-center mb-12">
-              <h2 className="text-3xl font-bold mb-4">Seyahat Temaları</h2>
-              <p className="text-gray-600 text-lg">Öne Çıkan Seyahat Temalarını İnceleyin</p>
+              <h2 className="text-3xl font-bold mb-2 text-black/80">Otel Temaları</h2>
+              <p className="text-gray-600 text-lg">Öne Çıkan Otel Temalarını İnceleyin</p>
             </div>
             
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
